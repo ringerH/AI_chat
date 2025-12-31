282aaec2536174bdbad0b6ef50fcbb83ad1d5773
ringerH <delahuntim@gmail.com>
2025-12-31 18:29:21 +0530

    Favicon and title

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,12 +1,15 @@
 <script lang="ts">
-	import "../app.css";
-	import favicon from '$lib/assets/favicon.svg';
+    import "../app.css";
+    import favicon from '$lib/assets/favicon.svg';
+    import faviconIco from '$lib/assets/favicon.ico';
 
-	let { children } = $props();
+    let { children } = $props();
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
+	<title>Acme Support</title>
+    <link rel="icon" href={faviconIco} sizes="any" />
+    <link rel="icon" href={favicon} type="image/svg+xml" />
 </svelte:head>
 
 {@render children()}
